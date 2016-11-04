96da821a0de2780f7ad3b180d3f9ed2f7a8fb20b
Eliomar Rodríguez <Eliomar Rodrguez>
2016-11-04 11:35:30 -0600

    Insertar ciudades y rutas entre ciudades

--- ProyectoEstructuras/main.cpp
+++ ProyectoEstructuras/main.cpp
@@ -15,8 +15,53 @@ struct vertice{
     bool visitado;
 }*grafo;
 
+void insertarCiudad(char pciudad){
+
+struct vertice *nnv = new vertice();
+nnv->ciudad=pciudad;
+nnv->visitado =false;
+
+nnv->sigV=grafo;
+grafo =nnv;
+}
+
+void insertarRutas(char porigen,char pdestino, int pdistancia){
+
+    struct vertice *origen =buscar (porigen);
+    struct vertice *destino =buscar (pdestino);
+    if((origen ==NULL )||(destino ==NULL)){
+        cout << "Datos inconsistentes\n";
+        return;
+    }
+
+    struct arco *nna = new arco();
+    nna->destino=destino;
+    nna->distancia=pdistancia;
+
+    nna->sigA =origen->sigA;
+    origen->sigA=nna;
+}
+
 int main()
 {
+    insertarCiudad('H');
+    insertarCiudad('F');
+    insertarCiudad('E');
+    insertarCiudad('D');
+    insertarCiudad('C');
+    insertarCiudad('B');
+    insertarCiudad('A');
+    insertarRutas('A','C',7);
+    insertarRutas('A','D',2);
+    insertarRutas('E','F',3);
+    insertarRutas('D','E',4);
+    insertarRutas('C','D',5);
+    insertarRutas('B','C',6);
+    insertarRutas('A','B',7);
+    insertarRutas('D','H',8);
+    insertarRutas('F','H',1);
+
+
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
     sf::CircleShape shape(200.f);
 
