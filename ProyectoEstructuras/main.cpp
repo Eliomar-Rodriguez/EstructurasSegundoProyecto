032c8d280a3690d70e83325d6999c222eeb87a9c
Eliomar Rodríguez <Eliomar Rodrguez>
2016-11-04 21:42:53 -0600

    agregando rutas

--- ProyectoEstructuras/main.cpp
+++ ProyectoEstructuras/main.cpp
@@ -59,10 +59,8 @@ void insertarRutas(char porigen[],char pdestino[], int pdistancia){
     origen->sigA=nna;
 }
 
-
-int main()
-{
-    // 25 ciudades
+void cargarDatos(){
+    // 32 ciudades
     insertarCiudad("San Jose");
     insertarCiudad("Naranjo");
     insertarCiudad("Palmares");
@@ -98,9 +96,70 @@ int main()
     insertarCiudad("Manzanillo");
 
 
-
     insertarRutas("Los Chiles Frontera","Santa Rosa",304);
+
+    insertarRutas("Santa Rosa","Los Chiles Frontera",304);
     insertarRutas("Santa Rosa","Liberia",73);
+    insertarRutas("Santa Rosa","Tamarindo",9);
+
+    insertarRutas("Tamarindo","Santa Rosa",9);
+    insertarRutas("Tamarindo","Liberia",78);
+    insertarRutas("Tamarindo","Playa Hermosa",72);
+
+    insertarRutas("Liberia","Tamarindo",78);
+    insertarRutas("Liberia","Santa Rosa",73);
+    insertarRutas("Liberia","Santa Cruz",58);
+    insertarRutas("Liberia","Tempisque",29);
+
+    insertarRutas("Playa Hermosa","Nicoya",74);
+    insertarRutas("Playa Hermosa","Cabuya",206);
+    insertarRutas("Playa Hermosa","Tamarindo",72);
+
+    insertarRutas("Nicoya","Playa Hermosa",74);
+    insertarRutas("Nicoya","Paquera",96);
+
+    insertarRutas("Cabuya","Playa Hermosa",206);
+    insertarRutas("Cabuya","Paquera",47);
+
+    insertarRutas("Paquera","Nicoya",96);
+    insertarRutas("Paquera","Cabuya",47);
+    insertarRutas("Paquera","Tempisque",150);
+    insertarRutas("Paquera","Manzanillo",105);
+
+    insertarRutas("Tempisque","Paquera",150);
+    insertarRutas("Tempisque","Liberia",29);
+    insertarRutas("Tempisque","Santa Cruz",85);
+
+    insertarRutas("Santa Cruz","Tempisque",85);
+    insertarRutas("Santa Cruz","Liberia",58);
+    insertarRutas("Santa Cruz","Fortuna",103);
+
+    insertarRutas("Fortuna","Santa Cruz",103);
+    insertarRutas("Fortuna","Sucre",52);
+
+    insertarRutas("Manzanillo","Palmares",93);
+    insertarRutas("Manzanillo","Paquera",105);
+
+    insertarRutas("Sucre","Fortuna",52);
+    insertarRutas("Sucre","Zarcero",22);
+
+    insertarRutas("Zarcero","Sucre",22);
+    insertarRutas("Zarcero","Naranjo",19);
+
+    insertarRutas("Naranjo","Zarcero",19);
+    insertarRutas("Naranjo","Palmares",12);
+    insertarRutas("Naranjo","Alajuela",19);
+    insertarRutas("Naranjo","San Jose",47);
+
+    insertarRutas("Palmares","Manzanillo",93);
+    insertarRutas("Palmares","Naranjo",12);
+    insertarRutas("Palmares","Jaco",83);
+
+    insertarRutas("Jaco","Palmares",83);
+    insertarRutas("Jaco","Parrita",42);
+
+    insertarRutas("Parrita","Jaco",42);
+    insertarRutas("Parrita","",0);
     insertarRutas("","",0);
     insertarRutas("","",0);
     insertarRutas("","",0);
@@ -120,19 +179,11 @@ int main()
     insertarRutas("","",0);
     insertarRutas("","",0);
     insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
-    insertarRutas("","",0);
+}
+
+int main()
+{
+
 
 
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
